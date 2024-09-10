287c182c0162c2edda0c8584a529810e5d36eeb6
Kaio <lamonthgames73@gmail.com>
2024-09-10 20:23:20 -0300

    Update and rename Aluno.java to Pessoa.java

---
+++ src/Pessoa.java
@@ -0,0 +1,40 @@
+package br.edu.ifs.academico;
+
+public class Pessoa {
+	private String nome;
+	private String localNascimento;
+	private String dataNascimento;
+	private char sexo;
+	
+	
+	public Pessoa(String nome) {
+		super();
+		this.setNome(nome);
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLocalNascimento() {
+		return localNascimento;
+	}
+	public void setLocalNascimento(String localNascimento) {
+		this.localNascimento = localNascimento;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public char getSexo() {
+		return sexo;
+	}
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+}
