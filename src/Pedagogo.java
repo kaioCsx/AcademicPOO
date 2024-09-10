2dfbe18f354681ca9cd31a0639ce1855ae7601c1
Kaio <lamonthgames73@gmail.com>
2024-09-10 20:28:56 -0300

    Create Pedagogo.java

---
+++ src/Pedagogo.java
@@ -0,0 +1,24 @@
+package br.edu.ifs.academico;
+
+public class Pedagogo extends Pessoa {
+	public Pedagogo(String nome) {
+		super(nome);
+		
+	}
+	private String experiencia;
+	private String faixaClasse;
+	
+	public String getExperiencia() {
+		return experiencia;
+	}
+	public void setExperiencia(String experiencia) {
+		this.experiencia = experiencia;
+	}
+	public String getFaixaClasse() {
+		return faixaClasse;
+	}
+	public void setFaixaClasse(String faixaClasse) {
+		this.faixaClasse = faixaClasse;
+	}
+
+}
