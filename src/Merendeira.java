b30c52ecd323ac85b6976c3d0673dbd7097d344d
Kaio <lamonthgames73@gmail.com>
2024-09-10 20:28:08 -0300

    Create Merendeira.java

---
+++ src/Merendeira.java
@@ -0,0 +1,24 @@
+package br.edu.ifs.academico;
+
+public class Merendeira extends Pessoa {
+	public Merendeira(String nome) {
+		super(nome);
+		
+	}
+	private String especialidade;
+	private String vulgo;
+	
+	public String getEspecialidade() {
+		return especialidade;
+	}
+	public void setEspecialidade(String especialidade) {
+		this.especialidade = especialidade;
+	}
+	public String getVulgo() {
+		return vulgo;
+	}
+	public void setVulgo(String vulgo) {
+		this.vulgo = vulgo;
+	}
+	
+}
