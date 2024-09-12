8d803e3a137c8edbc03afb4a290b632aa99db59b
Kaio <lamonthgames73@gmail.com>
2024-09-12 14:35:16 -0300

    Create Menu.java

---
+++ ArrayList/Menu.java
@@ -0,0 +1,28 @@
+package br.edu.ifs.academico;
+
+public class Menu {
+	public String getMenu() {
+		
+		return    "********************** \n"
+				+ " Sistema acadêmico IFS \n"
+				+ "        MENU         \n"
+				+ "********************** \n"
+				+ "Selecione a opcão: \n"
+				+ "1. Cadastrar Aluno \n"
+				+ "2. Imprimir lista Alunos \n"
+				+ "3. Cadastrar Professor \n"
+				+ "4. Imprimir lista Professores \n"
+				+ "5. Cadastrar Psicologo \n"
+				+ "6. Imprimir lista Psicologos \n"
+				+ "7. Cadastrar Pedagogo \n"
+				+ "8. Imprimir lista Pedagogos \n"
+				+ "9. Cadastrar Tecnico \n"
+				+ "10. Imprimir lista Tecnicos \n"
+				+ "11. Cadastrar Merendeira \n"
+				+ "12. Imprimir lista Merendeiras \n"
+				+ "13. Cadastrar Vigilante \n"
+				+ "14. Imprimir lista Vigilantes \n"
+				+ "0. Sair";
+	}
+  
+}
